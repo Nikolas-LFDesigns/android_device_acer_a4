42508d4541161c7f1361408422c38f6537064196
Nikolas <Lens.dpi@gmail.com>
2012-06-07 08:56:52 +0700

    introducing AcerQualcommRIL

--- acer/a4/dev/Apply-Patch.sh
+++ acer/a4/dev/Apply-Patch.sh
@@ -7,3 +7,4 @@ patch -p1 < $src_path/device/acer/a4/dev/bootanimFix.diff
 #camera patches
 patch -p1 < $src_path/device/acer/a4/dev/MemHeapBase.diff
 patch -p1 < $src_path/device/acer/a4/dev/Overlay.diff
+cp $src_path/device/acer/a4/dev/AcerQualcommRIL.java $src_path/frameworks/base/telephony/java/com/android/internal/telephony/AcerQualcommRIL.java
