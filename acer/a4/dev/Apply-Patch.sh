c6235a14eb7311fb884124d34fc51bad642cf135
Nikolas <Lens.dpi@gmail.com>
2012-06-22 12:33:33 +0700

    goodbye, patches!

--- acer/a4/dev/Apply-Patch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-src_path=~/android-ics/system
-cd $src_path
-
-patch -p1 < $src_path/device/acer/a4/dev/Liquid_build-patch.diff
-cp $src_path/device/acer/a4/dev/AcerQualcommRIL.java $src_path/frameworks/base/telephony/java/com/android/internal/telephony/AcerQualcommRIL.java
